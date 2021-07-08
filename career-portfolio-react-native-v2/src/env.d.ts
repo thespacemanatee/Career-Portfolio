2ac412d10a72adb0171b31ef80b3f428bb894b9e
Chee Kit <cheekit.chong98@gmail.com>
2021-07-08 23:31:31 +0800

    Added missing .env declaration file

---
+++ career-portfolio-react-native-v2/src/env.d.ts
@@ -0,0 +1,6 @@
+declare module "react-native-dotenv" {
+  export const ONET_USERNAME: string;
+  export const ONET_PASSWORD: string;
+  export const ONET_ENDPOINT: string;
+  export const RESULTS_ENDPOINT: string;
+}

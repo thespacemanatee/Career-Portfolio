772081436bdcaed2468da34fa05ff89f07570658
Chee Kit <cheekit.chong98@gmail.com>
2021-07-16 02:02:29 +0800

    Added empty history animation and call animation on focus screen

--- career-portfolio-react-native-v2/src/screens/PastResultsScreen.tsx
+++ career-portfolio-react-native-v2/src/screens/PastResultsScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Alert, StyleSheet, View } from "react-native";
+import { Alert, Dimensions, StyleSheet, View } from "react-native";
 import { Button, Layout } from "@ui-kitten/components";
 import { ScrollView } from "react-native-gesture-handler";
 
@@ -7,24 +7,34 @@ import ScreenTitle from "../components/ScreenTitle";
 import useFetchSubmissions from "../helpers/hooks/useFetchSubmissions";
 import { ResultsLocalStorageItem } from "../types";
 import ResultsOverviewCard from "../components/ResultsOverviewCard";
+import { useAppDispatch } from "../app/hooks";
+import { setAllTasks } from "../app/features/tasks/tasksSlice";
+import { LottieView } from "..";
 
 const PastResultsScreen = ({ navigation }) => {
   const [entries, setEntries] = useState<ResultsLocalStorageItem[]>(null);
 
+  const { height } = Dimensions.get("window");
+
   const { result, resetSubmissions } = useFetchSubmissions();
 
+  const dispatch = useAppDispatch();
+
   const handleNavigateResults = (id: string) => {
+    dispatch(setAllTasks(entries[id].payload.task_list));
     navigation.navigate("SubmitLoading", { payload: entries[id].payload });
   };
 
   const handleResetSubmissions = () => {
     Alert.alert("Are you sure?", "This action will delete all your entries.", [
+      { text: "Cancel", style: "cancel" },
       {
         text: "Delete",
         style: "destructive",
-        onPress: resetSubmissions,
+        onPress: async () => {
+          await resetSubmissions();
+        },
       },
-      { text: "Cancel", style: "cancel" },
     ]);
   };
 
@@ -46,9 +56,20 @@ const PastResultsScreen = ({ navigation }) => {
           </Button>
         )}
       </View>
-      <ScrollView>
-        {entries &&
-          Object.keys(entries).map((id, index) => {
+      {!entries && (
+        <View style={styles.emptyComponent}>
+          <LottieView
+            // eslint-disable-next-line global-require
+            source={require("../../assets/empty_history.json")}
+            autoPlay
+            loop
+            style={{ height: height / 3 }}
+          />
+        </View>
+      )}
+      {entries && (
+        <ScrollView>
+          {Object.keys(entries).map((id, index) => {
             return (
               <View key={id} style={styles.cardContainer}>
                 <ResultsOverviewCard
@@ -61,7 +82,8 @@ const PastResultsScreen = ({ navigation }) => {
               </View>
             );
           })}
-      </ScrollView>
+        </ScrollView>
+      )}
     </Layout>
   );
 };
@@ -78,5 +100,10 @@ const styles = StyleSheet.create({
   },
   cardContainer: {
     marginBottom: 16,
+  },
+  emptyComponent: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
